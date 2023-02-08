e180ef51a8a59dc9eedd270ed557e9c3906f4fd8
jarrel <jarrel.folly-adjon@edu.devinci.fr>
2023-02-08 09:05:54 +0100

    fix and add

---
+++ script/projet.js
@@ -0,0 +1,20 @@
+console.clear();
+
+// selectionne l'élement qui a pour id app
+const elApp = document.querySelector("#app");
+
+// What we need...
+// Mouse position
+// Currently hovered item
+
+
+// eccoute la page sur l'élément body sur l'évenemnt bouger la sourie
+document.body.addEventListener("mousemove", (event) => {
+  console.log({
+    x: event.clientX,
+    y: event.clientY
+  });
+
+  elApp.style.setProperty("--x", event.clientX);
+  elApp.style.setProperty("--y", event.clientY);
+});
