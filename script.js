0d920ffa877dac71001860bdb78f5982aad76756
jarrel <jarrel.folly-adjon@edu.devinci.fr>
2023-03-02 19:50:12 +0100

    title anime

--- script.js
+++ script.js
@@ -29,3 +29,11 @@ function togglee(){
 
 
 
+let docTitle = document.title;
+window.addEventListener("blur", () => {
+  document.title = "Come Back ;(";
+});
+
+window.addEventListener("focus", () => {
+  document.title = docTitle;
+});
